c95ea5eceee11cc0eb7b3498eb5a987d0e955434
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-04-14 17:05:20 -0500

    se añade la funcion de log base 10

--- app/calc.py
+++ app/calc.py
@@ -1,19 +1,22 @@
 import app
-
+import math
 
 class InvalidPermissions(Exception):
     pass
 
 
 class Calculator:
+    #add
     def add(self, x, y):
         self.check_types(x, y)
         return x + y
 
+    #substract
     def substract(self, x, y):
         self.check_types(x, y)
         return x - y
 
+    #multiply
     def multiply(self, x, y):
         if not app.util.validate_permissions(f"{x} * {y}", "user1"):
             raise InvalidPermissions('User has no permissions')
@@ -21,6 +24,7 @@ class Calculator:
         self.check_types(x, y)
         return x * y
 
+    #divide
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
@@ -28,14 +32,31 @@ class Calculator:
 
         return x / y
 
+    #power
     def power(self, x, y):
         self.check_types(x, y)
         return x ** y
     
+    #square_root
     def square_root(self, x):
         self.check_types_single(x)
-        return x ** (1/2)
 
+        if x < 0:
+            raise TypeError("It's not possible square root for negative numbers")
+        
+        return x ** (1/2)
+    
+    #log base 10
+    def log_base_10(self, x):
+        self.check_types_single(x)
+        
+        if x <= 0:
+            raise TypeError("It's not possible log base 10 for negative numbers including zero")
+        
+        result = math.log10(x)
+        return result
+
+    #validators
     def check_types(self, x, y):
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
             raise TypeError("Parameters must be numbers")
